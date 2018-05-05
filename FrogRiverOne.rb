9d12ab0f6d04976d52f799a96218c7b3cd34fbae
Galian <galian5@wp.pl>
2018-05-05 20:01:39 +0200

    Correct FrogRiverOne to get 100% performance

--- FrogRiverOne.rb
+++ FrogRiverOne.rb
@@ -1,15 +1,20 @@
-#low performance, need to rewrite. Don't want to now
+#bit ugly, but works
 def solution(x, a)
-  b=[*1..x]
-  score = -1
-  a.each_with_index do |value, index|
-    b = b-[value]
-    if b.empty?
-      score = index
-      break
+  b=[1]*(a.length+1)
+
+  a.each_with_index do |element,index |
+    if b[element]==1
+      b[element]=0
+      x -= 1
     end
+    if x==0
+      return index
+
+    end
+
   end
-  puts score
+-1
 end
 
 puts solution(5,[1,3,1,4,2,3,5,4])
+puts solution(1,[1])
