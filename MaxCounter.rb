9398e11ce014fca05357059b77a3d7756f8a676b
Galian <galian5@wp.pl>
2018-07-16 23:05:01 +0200

    one test in maxcounter doesn't pass. no idea

--- MaxCounter.rb
+++ MaxCounter.rb
@@ -1,14 +1,13 @@
 def solution(n, a)
   counters = Array.new(n, 0)
+  max = 0
   a.each do |value|
     if value <= n
-      counters[value] = counters[value] + 1
+      counters[value-1] = counters[value-1] + 1
+      max = counters[value-1] if counters[value-1] > max
     else
-      #should have variable with max value, don't use max
-      counters = Array.new(n, counters.max)
+      counters = Array.new(n, max)
     end  
   end
 counters
 end
-
-
